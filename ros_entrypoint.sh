3271ea33667d487e177250191d110a96586f09f8
Max Polzin <polzin.max@gmail.com>
2023-08-25 16:36:34 +0200

    Update ros_entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID} --uid ${GID} ros
+id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} --uid ${GID:=1000} ros
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /colcon_ws/install/setup.bash
