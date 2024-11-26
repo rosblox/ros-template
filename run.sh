012aad60802e2301a3da3875cc8d1087286670cf
Max Polzin <polzin.max@gmail.com>
2024-11-26 09:34:56 +0100

    Fix typo.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export HOST_UID=$(id -u)
 
 docker compose -f $SCRIPT_DIR/docker-compose.yml run \
---volume $(pwd)/livox_ros_driver2:/colcon_ws/src/livox_ros_driver2 \
---volume $(pwd)/Livox-SDK2:/Livox-SDK2 \
-ros-livox-ros-driver2 bash
+${REPOSITORY_NAME} bash
